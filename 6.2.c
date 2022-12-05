2d547ee40b201d7450207b5ce512cc5c7356d8cd
KatyaDemina03 <114250363+KatyaDemina03@users.noreply.github.com>
2022-12-05 22:55:08 +0200

    Add files via upload

---
+++ 6.2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SECONDS 3
+
+int main() {
+    pid_t pid;
+
+    printf("Forking started\n");
+    pid = fork();
+
+    int count = 0;
+    time_t currentTime = time(NULL);
+
+    if (pid == 0) {
+        while (time(NULL) != (currentTime + SECONDS)) {
+            count++;
+        }
+        printf("Child PID = %d. Count = %d\n", getpid(), count);
+    } else if (pid > 0) {
+        while (time(NULL) != (currentTime + SECONDS)) {
+            count++;
+        }
+        printf("Parent PID = %d. Count = %d\n", getpid(), count);
+    } else {
+        printf("Error\n");
+    }
+
+    return 0;
+}
+
